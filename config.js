22c213daea0fde4a1d31562154538076aedc72eb
BroDi4 <xrip73@mail.ru>
2023-06-19 12:47:30 +0400

    prepare to deploy

---
+++ config.js
@@ -0,0 +1,2 @@
+export const dbURI =
+  'mongodb+srv://brod:12BroD123@cluster.8z0udat.mongodb.net/sportstore?retryWrites=true&w=majority';
--- index.js
+++ index.js
@@ -16,22 +16,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//Connect to DB and create server
 mongoose
-  .connect(
-    'mongodb+srv://brod:12BroD21@cluster.8z0udat.mongodb.net/sportstore?retryWrites=true&w=majority',
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('DB ok'))
   .catch((err) => console.log(err));
 
-app.listen('4000', (err) => {
+app.listen(process.env.PORT || 4000, (err) => {
   if (err) {
     console.log(err);
   }
   console.log('Server started');
 });
-
-//Query
 
 app.get('/products', productController.getALl);
 
